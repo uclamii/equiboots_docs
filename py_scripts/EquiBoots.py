ee4d4890fae22604295a7cbd533f0da9cce7d6f6
elemets <afunnell@mednet.ucla.edu>
2025-03-20 11:44:42 -0700

    Updating init variables

--- py_scripts/EquiBoots.py
+++ py_scripts/EquiBoots.py
@@ -1,7 +1,8 @@
-
+import pandas as pd
+import numpy as np
 
 class EquiBoots:
     
-    def __init__(self, task: str ='binary_classification', fariness_vars: ) -> None:
+    def __init__(self, y_prob: np.array, y_true: np.array, fairness_df: pd.DataFrame, task: str ='binary_classification', fariness_vars: list=[None]) -> None:
         
         pass
